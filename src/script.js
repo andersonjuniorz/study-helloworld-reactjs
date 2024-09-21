2ebfa8837e37f51b0e2488f2bcbbb411d068a8bb
Andersonjuniorz <dev.andersonjunior@gmail.com>
2024-09-20 23:20:15 -0300

    Organizing lines with space

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import React from "https://esm.sh/react";
 import ReactDOM from "https://esm.sh/react-dom";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 class Clock extends React.Component {
   render() {
@@ -7,10 +8,11 @@ class Clock extends React.Component {
       <div>
         {" "}
         <h1>Olá, Mundo!</h1>
+        
         <h2>
-          Horário:
-          {this.props.date.toLocaleTimeString()}.
+          Horário: {this.props.date.toLocaleTimeString()}.
         </h2>
+
       </div>
     );
   }
@@ -19,4 +21,5 @@ class Clock extends React.Component {
 function tick() {
   root.render(<Clock date={new Date()} />);
 }
+
 setInterval(tick, 1000);
